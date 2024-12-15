a6cbffeba869b12ab9bc2a7e4a71d899465a69a8
truongbqhe160611 <buitruong091102@gmail.com>
2024-12-15 17:40:18 +0700

    fixforgot

--- src/features/auth/forgotpassword/ConfirmForgotPassword.tsx
+++ src/features/auth/forgotpassword/ConfirmForgotPassword.tsx
@@ -3,7 +3,7 @@ import ForgotPopUp from './ForgotPopUp';
 
 const ConfirmForgotPassword = () => {
     const currentUrl = window.location.href;
-    const isArgonautDomain = currentUrl.startsWith("http://localhost:3000/") || currentUrl.startsWith("http://localhost:3001/") || currentUrl.startsWith("http://localhost:3002/") || currentUrl.startsWith("https://trongnp-registry.site/");
+    const isArgonautDomain = currentUrl.startsWith("http://localhost:3000/") || currentUrl.startsWith("http://localhost:3001/") || currentUrl.startsWith("http://localhost:3002/") || currentUrl.startsWith("https://admin.trongnp-registry.site//");
     const urlParams = new URL(currentUrl).searchParams;
     const token = urlParams.get('token');
     const navigate = useNavigate();
